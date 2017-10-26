784ab5ad3404186f0a2b9971ef01c15abbe219ed
razvi786 <ehsan.razvi92@gmail.com>
2017-10-26 19:00:00 +0530

    Create index.php
    this is perfect just i need to make the login page more attractive

---
+++ index.php
@@ -0,0 +1,8 @@
+<form action="dice.php" method="POST" align="center">
+	<h1>Welcome To Dice Game.</h1><br>
+	<h3> Player 1: </h3>
+	<input type="text" name="player1" placeholder="Enter Your Name"><br>
+	<h3> Player 2: </h3>
+	<input type="text" name="player2" placeholder="Enter Your Name"><br><br>
+	<input type="submit" name="names" value="Let's Play"" >
+</form>
